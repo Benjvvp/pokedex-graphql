7047a183b11ec659f073e2f7235c70c245c0e9cd
Benjamin <BPerez51511a@gmail.com>
2023-12-11 21:10:15 -0300

    feat: Deje solo lo inicial de la tech

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,9 +2,4 @@ import SchemaBuilder from "@pothos/core";
 
 export const builder = new SchemaBuilder({});
 
-builder.queryType();
-builder.mutationType();
-
-import "./pokemonSchema";
-
 export const schema = builder.toSchema();
